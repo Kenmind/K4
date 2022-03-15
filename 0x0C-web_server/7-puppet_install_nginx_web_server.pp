9a8a8ba41b5e6780ab35f190e94ca6c503ded8ba
Kenmind <kennedykalaluka04@gmail.com>
2022-03-15 18:00:03 +0200

    Puppet puppet me

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,22 @@
 # installs and configures Nginx web server
-exec {'apt-get update':
-  path => '/usr/bin'
-}
+
 package {'nginx':
+  ensure => 'installed',
 }
-file {'index.html':
-  ensure  => 'present',
-  path    => '/etc/nginx/html/index.html',
+
+
+file_line {'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'Listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://youtube.com/UCw4X_zayaSiuVYcqWpiaSWw permanent;',
+}
+
+file {'/var/www/html/index.html':
   content => 'Hello World!',
 }
-file {'404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page",
-  path    => '/etc/nginx/html/404.html',
-}
-file {'default':
-  ensure  => 'present',
-  path    => '/etc/nginx/html/default',
-  content => "server {\n\tlisten 80;\n\tlisten [::]:80 default_server;\n\troot   /etc/nginx/html;\n\tindex  index.html index.htm;\n\tlocation /redirect_me {\n\t\treturn 301 http://youtube.com/UCw4X_zayaSiuVYcqWpiaSWw;\n\t}\n\terror_page 404 /404.html;\n\tlocation /404 {\n\t\troot /etc/nginx/html;\n\t\tinternal;\n\t }\n}"
+
+service {'nginx':
+  ensure  => 'running',
+  require => Package['nginx'],
 }
