a1533a65ce95193f6b0062a0b3939e085f4180fa
Kenmind <kennedykalaluka04@gmail.com>
2022-03-15 17:40:54 +0200

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,11 +2,7 @@
 exec {'apt-get update':
   path => '/usr/bin'
 }
-exec {'apt-get install -y nginx':
-  path => '/usr/bin'
-}
-exec {'mkdir':
-  path => '/etc/nginx/html'
+package {'nginx':
 }
 file {'index.html':
   ensure  => 'present',
@@ -22,7 +18,4 @@ file {'default':
   ensure  => 'present',
   path    => '/etc/nginx/html/default',
   content => "server {\n\tlisten 80;\n\tlisten [::]:80 default_server;\n\troot   /etc/nginx/html;\n\tindex  index.html index.htm;\n\tlocation /redirect_me {\n\t\treturn 301 http://youtube.com/UCw4X_zayaSiuVYcqWpiaSWw;\n\t}\n\terror_page 404 /404.html;\n\tlocation /404 {\n\t\troot /etc/nginx/html;\n\t\tinternal;\n\t }\n}"
-}
-exec {'service nginx restart':
-  path => '/usr/bin'
 }
