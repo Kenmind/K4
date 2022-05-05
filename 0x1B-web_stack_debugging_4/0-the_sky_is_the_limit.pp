e04d7782366fdb324e894c5ae1c7cc77bd4d6516
Kenmind <kennedykalaluka04@gmail.com>
2022-05-05 08:28:33 +0200

    no limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit.pp
@@ -1,4 +1,4 @@
 # Fixing limited requests problem 
-exec { "sed -i 's/15/4096/' /etc/default/nginx && sudo service nginx restart":
+exec { "sed -i 's/15/2000/' /etc/default/nginx && sudo service nginx restart":
 path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
 }
